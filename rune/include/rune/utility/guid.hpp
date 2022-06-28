47adca03f782a1af62c79997953ae1c825152374
Stuart Millman <stu.millman15@gmail.com>
2022-06-28 13:46:45 +0100

    Add Guid

---
+++ rune/include/rune/utility/guid.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+#include "rune/defines.hpp"
+
+namespace Rune
+{
+    class Guid
+    {
+    public:
+        Guid();
+        Guid(u64 guid);
+        Guid(const Guid& other) = default;
+
+        operator u64() const;
+
+    private:
+        u64 m_guid;
+    };
+}
+
+namespace std
+{
+    template <>
+    struct hash<Rune::Guid>
+    {
+        auto operator()(const Rune::Guid& guid) const noexcept -> std::size_t
+        {
+            return hash<u64>()(guid);
+        }
+    };
+}
---
+++ rune/src/utility/guid.cpp
@@ -0,0 +1,15 @@
+#include "rune/utility/guid.hpp"
+
+#include "rune/maths/random.hpp"
+
+namespace Rune
+{
+    Guid::Guid() : m_guid(Random::randomLong()) {}
+
+    Guid::Guid(const u64 guid) : m_guid(guid) {}
+
+    Guid::operator u64() const
+    {
+        return m_guid;
+    }
+}
