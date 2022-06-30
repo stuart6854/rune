51992d60420f3e31ff1127c1d57ecaf65e70de72
Stuart Millman <stu.millman15@gmail.com>
2022-06-30 21:07:13 +0100

    Switch to OpenGL 4.6

--- rune/src/core/window.cpp
+++ rune/src/core/window.cpp
@@ -139,7 +139,7 @@ namespace Rune
 
         glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
         glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 5);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
 
         glfwWindowHint(GLFW_AUTO_ICONIFY, GLFW_FALSE);
 
