516e55dedc240ec045f51e70b1792a2ecaf7c9ce
Stuart Millman <stu.millman15@gmail.com>
2022-07-23 21:09:39 +0100

    Calling C++ from C#

--- rune_scriptcore/Source/main.cs
+++ rune_scriptcore/Source/main.cs
@@ -1,7 +1,20 @@
 ﻿using System;
+using System.Runtime.CompilerServices;
 
 namespace Rune
 {
+    public struct Vector3
+    {
+        public float x, y, z;
+
+        public Vector3(float x = 0, float y = 0, float z = 0)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+    }
+
     public class Main
     {
         public float FloatVar { get; set; }
@@ -9,6 +22,13 @@ namespace Rune
         public Main()
         {
             Console.WriteLine("Main constructor!");
+
+            NativeLog("Stuart", 24);
+
+            Vector3 pos = new Vector3(5, 2.5f, 1);
+            NativeLog_Vector3(ref pos, out Vector3 result);
+            Console.WriteLine($"{result.x},{result.y},{result.z}");
+            Console.WriteLine("{0}", NativeLog_Vector3Dot(ref pos));
         }
 
         public void PrintMessage()
@@ -25,5 +45,14 @@ namespace Rune
         {
             Console.WriteLine($"C# says: {message}");
         }
+
+        [MethodImpl(MethodImplOptions.InternalCall)]
+        static extern void NativeLog(string text, int parameter);
+
+        [MethodImpl(MethodImplOptions.InternalCall)]
+        static extern void NativeLog_Vector3(ref Vector3 param, out Vector3 outResult);
+
+        [MethodImpl(MethodImplOptions.InternalCall)]
+        static extern float NativeLog_Vector3Dot(ref Vector3 param);
     }
 }
